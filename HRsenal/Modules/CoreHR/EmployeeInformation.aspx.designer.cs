7d0c23834d2c028f9cb94c5b36f4831d4db65fea
murtaza-zaidi <murtaza.abbas07094@khi.iba.edu.pk>
2016-12-30 03:09:07 +0500

    EmpInfoFinal

--- HRsenal/Modules/CoreHR/EmployeeInformation.aspx.designer.cs
+++ HRsenal/Modules/CoreHR/EmployeeInformation.aspx.designer.cs
@@ -139,6 +139,15 @@ namespace HRsenal {
         protected global::System.Web.UI.WebControls.GridView GridDiscipline;
         
         /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
