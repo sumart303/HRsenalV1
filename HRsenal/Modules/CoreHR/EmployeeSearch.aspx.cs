3043a42d2c022b3d41f387ce68b6575854ce7e52
sumart303 <s.umartariq@live.com>
2016-12-29 23:32:22 +0500

    Employee Search

---
+++ HRsenal/Modules/CoreHR/EmployeeSearch.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace HRsenal.Modules.CoreHR
+{
+    public partial class EmployeeSearch : System.Web.UI.Page
+    {
+        DataTable EmployeeSearchResults;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadResults();
+        }
+
+        private void LoadResults()
+        {
+            EmployeeSearchResults = new DataTable();
+            EmployeeSearchResults.Columns.Add(new DataColumn("EmpId"));
+            EmployeeSearchResults.Columns.Add(new DataColumn("EmpName"));
+            GridEmpSearchResults.DataSource = EmployeeSearchResults;
+            GridEmpSearchResults.DataBind();
+        }
+    }
+}
